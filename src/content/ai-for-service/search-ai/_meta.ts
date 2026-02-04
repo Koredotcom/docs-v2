cb126f22f3ce4c82d28d4fbafb0fa647948fa31b
Prabhat Singh <109069787+prabhat-singh-kore@users.noreply.github.com>
2026-02-04 10:09:19 +0530

    faq, dev tools, how-tos

--- src/content/ai-for-service/search-ai/_meta.ts
+++ src/content/ai-for-service/search-ai/_meta.ts
@@ -5,4 +5,8 @@ export default {
   'retrieval-answer-configuration': 'Retrieval & Answer Configuration',
   'agentic-rag-and-business-rules': 'Agentic RAG & Business Rules',
   'testing-and-debugging': 'Testing & Debugging',
+  'advance-config-and-dev-tools': 'Dev Tools',
+  'faq': 'FAQs',
+  'how-to': 'How Tos',
+  'best-practices': 'Best Practices'
 }
